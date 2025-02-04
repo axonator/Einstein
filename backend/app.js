28353b19149016c773f30e789c534272b424b81e
omkarAxonator <omkar@axonator.com>
2025-02-04 16:01:23 +0530

    hide local host

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ exports.handler = (event, context) => {
 
 
 // Start LOCAL server
-const PORT = 5000;
-app.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`);
-});
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//  console.log(`Server running on port ${PORT}`);
+// });
