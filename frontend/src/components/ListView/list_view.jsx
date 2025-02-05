502ee275bda1b81c60deb70dc5b6dc1b7af91900
omkarAxonator <omkar@axonator.com>
2025-02-05 15:27:32 +0530

    resolved add project issue

--- frontend/src/components/ListView/list_view.jsx
+++ frontend/src/components/ListView/list_view.jsx
@@ -19,6 +19,10 @@ const Listview = ({taskDetails,taskTypeCode,filtertasktype,parenntId,selectedTab
   const [pageSize, setPageSize] = useState(5);
   const [totalTasks, settotalTasks] = useState(0);
   const [totalPages, setTotalPages] = useState(0)
+  const [filterList,setfilterList] = useState([]);
+  const [appliedFilter, setappliedFilter] = useState("Filters");
+  const [dropdownOptions, setDropdownOptions] = useState({});
+
 
   useEffect(()=>{
     sortOrder=='asc'?setTasks(asctasks):setTasks(descTasks)
@@ -41,16 +45,43 @@ const Listview = ({taskDetails,taskTypeCode,filtertasktype,parenntId,selectedTab
   }, [taskTypeCode, parenntId]); // Trigger on either variable change
   
   
-    useEffect(() => {
-      setCurrentPage(1);
-      async function availableCustomFields() {
-        const data = await getcustomFields(selectedTabId,setError,setLoading);
-        setavailableCustomFields(data)
+  useEffect(() => {
+    setCurrentPage(1);
+    async function availableCustomFields() {
+      const data = await getcustomFields(selectedTabId,setError,setLoading);
+      setavailableCustomFields(data)
+      const newFilterList = data.filter(filter =>
+        filter['type'] == 'choice'
+      )
+      setfilterList(newFilterList)
+    }
+    availableCustomFields();
+    fetchTasks()
+    
+  }, [selectedTabId]);
+
+  const fetchcustomdropdownlist = async (table_name, column_name="*",condition='',listName) => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_LOCAL_URL}/api/tasks/get_list`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ table_name: table_name, column_name: column_name,condition:condition }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch ${table_name}: ${response.status}`);
       }
-      availableCustomFields();
-      fetchTasks()
-      
-    }, [selectedTabId]);
+      const requestedList = await response.json();
+      setDropdownOptions((prevalue)=>({
+        ...prevalue,[listName]:requestedList
+      }));
+    } catch (error) {
+      console.error(error);
+      alert(`Error fetching ${table_name} list`);
+    }
+  };
 
   // Fetch function to call the API
   const fetchTasks = async () => {
@@ -111,6 +142,17 @@ const Listview = ({taskDetails,taskTypeCode,filtertasktype,parenntId,selectedTab
     setCurrentPage(parseInt(e.target.value, 10));
   };
 
+  const handelfilter = async (e) => {
+    const display_name_singular = e.target.value;
+    const cf_id = e.target.options[e.target.selectedIndex].id;
+    await setappliedFilter(display_name_singular);
+    await fetchcustomdropdownlist('lookup', "*", "WHERE fk_custom_field_id=" + cf_id, display_name_singular);
+  };
+
+  // useEffect(()=>[
+
+  // ])
+
   // Handle modal toggle
   const toggleModal = () => {
     setShowModal(!showModal);
@@ -127,8 +169,9 @@ const Listview = ({taskDetails,taskTypeCode,filtertasktype,parenntId,selectedTab
       setCurrentPage(currentPage - 1);
     }
   };
-  
 
+  // console.log(dropdownOptions);
+  
   return loading ? 
     (
     <div className='container d-flex justify-content-center'>
@@ -146,73 +189,90 @@ const Listview = ({taskDetails,taskTypeCode,filtertasktype,parenntId,selectedTab
         <button className="btn btn-primary mb-1" onClick={toggleModal}>Add {selectedTabname} +</button>
         
 
-        {totalTasks > 0 ?
         <div id='list'>
           <div className="d-flex justify-content-between align-items-center">
             {/* Sorting Dropdown */}
-            <div className='d-flex justify-content-between align-items-center'>
-              <div className="d-flex align-items-center me-2">
-                {/* <label className="me-2">Sort by:</label> */}
-                <select 
-                  className="form-select w-auto" 
-                  value={sortOrder} 
-                  data-bs-toggle = "tooltip"
-                  title='Sort Order'
-                  onChange={(e) => setSortOrder(e.target.value)}
-                  >
-                  <option value="asc">Ascending</option>
-                  <option value="desc">Descending</option>
-                </select>
-              </div>
+            {totalPages > 0 && 
+              <div className='d-flex justify-content-between align-items-center'>
+                <div className="d-flex align-items-center me-2">
+                  {/* <label className="me-2">Sort by:</label> */}
+                  <select 
+                    className="form-select w-auto" 
+                    value={sortOrder} 
+                    data-bs-toggle = "tooltip"
+                    title='Sort Order'
+                    onChange={(e) => setSortOrder(e.target.value)}
+                    >
+                    <option value="asc">Ascending</option>
+                    <option value="desc">Descending</option>
+                  </select>
 
-              <div className="d-flex align-items-center">
-                {/* <label className="me-2">Page Size</label> */}
-                <select 
-                  className="form-select w-auto" 
-                  value={pageSize} 
-                  onChange={handlePageSize} 
-                  data-bs-toggle = "tooltip"
-                  title='Number of Records'
-                >
-                  <option value="5">5</option>
-                  <option value="10">10</option>
-                  <option value="15">15</option>
-                  <option value="20">20</option>
-                  <option value={totalTasks}>All</option>
-                </select>
+                  <select className="form-select w-auto ms-2" value={appliedFilter} onChange={handelfilter}>
+                    {filterList.map((item, index) => (
+                      <option key={index} value={item.display_name_singular} id={item.custom_field_id}>
+                        {item.display_name_singular}
+                      </option>
+                    ))}
+                  </select>
+                  {appliedFilter != 'Filters' &&
+                    <select className="form-select w-auto ms-2" value={appliedFilter} onChange={handelfilter}>
+                      {dropdownOptions[appliedFilter].map((item, index) => (
+                        <option key={index} value={item.option} id={item.lookup_id}>
+                          {item.option}
+                        </option>
+                      ))}
+                    </select>
+                  }
+                </div>
+
+                <div className="d-flex align-items-center">
+                  {/* <label className="me-2">Page Size</label> */}
+                  <select 
+                    className="form-select w-auto" 
+                    value={pageSize} 
+                    onChange={handlePageSize} 
+                    data-bs-toggle = "tooltip"
+                    title='Number of Records'
+                  >
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="20">20</option>
+                    <option value={totalTasks}>All</option>
+                  </select>
+                </div>
               </div>
-            </div>
+            }
 
             {/* Pagination controls */}
-          {totalPages == 1 ? 
-            null:
-            <div className="d-flex justify-content-end align-items-center">
-              <button
-                className="btn btn-outline-primary m-2"
-                onClick={prevPage}
-                data-bs-toggle = "tooltip"
-                title='Previous Page'
-                disabled={currentPage === 1}
-              >
-                <i className="bi bi-arrow-left-circle"></i>
-              </button>
-              <span>{(currentPage - 1) * pageSize +1} - {pageSize * currentPage} of {totalTasks}</span>
-              <select className="form-select w-auto ms-2" value={currentPage} onChange={handlePageJump}>
-                {[...Array(totalPages)].map((_, i) => (
-                  <option key={i + 1} value={i + 1}>{i + 1}</option>
-                ))}
-              </select>
-              <button
-                className="btn btn-outline-primary m-2"
-                onClick={nextPage}
-                data-bs-toggle="tooltip"
-                title = 'Next Page'
-                disabled={currentPage == totalPages}
-              >
-                <i className="bi bi-arrow-right-circle"></i>
-              </button>
-            </div>
-           }
+            {totalPages > 0 &&
+              <div className="d-flex justify-content-end align-items-center">
+                <button
+                  className="btn btn-outline-primary m-2"
+                  onClick={prevPage}
+                  data-bs-toggle = "tooltip"
+                  title='Previous Page'
+                  disabled={currentPage === 1}
+                >
+                  <i className="bi bi-arrow-left-circle"></i>
+                </button>
+                <span>{(currentPage - 1) * pageSize +1} - {pageSize * currentPage} of {totalTasks}</span>
+                <select className="form-select w-auto ms-2" value={currentPage} onChange={handlePageJump}>
+                  {[...Array(totalPages)].map((_, i) => (
+                    <option key={i + 1} value={i + 1}>{i + 1}</option>
+                  ))}
+                </select>
+                <button
+                  className="btn btn-outline-primary m-2"
+                  onClick={nextPage}
+                  data-bs-toggle="tooltip"
+                  title = 'Next Page'
+                  disabled={currentPage == totalPages}
+                >
+                  <i className="bi bi-arrow-right-circle"></i>
+                </button>
+              </div>
+            }
               
           </div>
 
@@ -262,10 +322,20 @@ const Listview = ({taskDetails,taskTypeCode,filtertasktype,parenntId,selectedTab
            }
 
           {/* Modal for adding a new task */}
-          {showModal && (<FormComponent toggleModal={toggleModal} refreshTasks={fetchTasks} parent_task_id={parenntId} selectedTabId={selectedTabId} selectedTabName={selectedTabname} availableCustomFields={availableCustomFields} taskToEdit={taskToEdit} totalPages={totalPages} setSortOrder={setSortOrder} setCurrentPage={setCurrentPage}/>)}
+          {showModal && 
+            (<FormComponent 
+              toggleModal={toggleModal} 
+              refreshTasks={fetchTasks} 
+              parent_task_id={parenntId} 
+              selectedTabId={selectedTabId} 
+              selectedTabName={selectedTabname} 
+              availableCustomFields={availableCustomFields} 
+              taskToEdit={taskToEdit} 
+              totalPages={totalPages} 
+              setSortOrder={setSortOrder} 
+              setCurrentPage={setCurrentPage} 
+              />)}
         </div>
-        : null}
-
       </div>
     );
 };
