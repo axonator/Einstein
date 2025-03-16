9e74947cf91a6a9550bb32b451781ac5b2ca7a16
omkarAxonator <omkar@axonator.com>
2025-03-16 21:03:23 +0530

    testing new build

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -348,7 +348,7 @@ export default function Contact({table_name="contact", columns = '*', condition 
     <Box sx={{ width: '100%' }} className="noshadow">
         
             <TextField
-                label="Search Contacts"
+                label="Search Contactsss"
                 variant="outlined"
                 size="small"
                 value={searchQuery}
